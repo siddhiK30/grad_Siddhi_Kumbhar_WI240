6934ca91d12c9dae0f795ce02ecbc83bfe739e5d
siddhiK30 <kumbharsiddhi30@gmail.com>
2026-02-23 20:35:47 +0530

    Multiple Databse Sping app

---
+++ Multiple-Databse/src/main/java/com/example/demo/config/DevConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Bean
+    public String myenvironment() {
+           System.out.println("🔥 DEV PROFILE IS ACTIVE");
+        return "Development Environment";
+    }
+}
+
